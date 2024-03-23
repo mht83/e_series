0347b04c0f79cc000a55fcb4c0becd9b7300ea36
Mohammadhassan Tohidi <tohidihassan9@gmail.com>
2024-03-23 15:14:40 +0330

    Add project files.

---
+++ e series/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace e_series
+{
+    internal class Program
+    {
+        public static int Factorial(int n)
+        {
+            return n == 0 ? 1 : n * Factorial(n - 1);
+        }
+
+        static void Main(string[] args)
+        {
+            decimal sum = 0;
+            for (int indexer = 0; indexer <= 33; indexer++)
+            {
+                sum += 1 / (decimal)Factorial(indexer);
+            }
+            Console.WriteLine(sum);
+            Console.WriteLine(Math.E);
+        }
+
+    }
+}
+
+
+
+
